f737271f86002c93c463dbd1c3d811c1d9b76913
Tawan Fonseca <webdevtawanfonseca@gmail.com>
2023-01-19 00:46:50 -0300

    basic css, font and link for images add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   ""
   );
 }
 
 export default App;
+
+
+
+/*
+ images links :
+ https://unsplash.com/pt-br/fotografias/_4qQsmE4VdA?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/YmyFBvW7oG8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/UC0HZdUitWY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/1Rm9GLHV0UA?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/XoByiBymX20?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/yr-sW_x9aHk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+ https://unsplash.com/pt-br/fotografias/SnsV0T5dWOk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+
+https://unsplash.com/pt/fotografias/6FbArnPXEVg?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+https://unsplash.com/pt-br/fotografias/4CrPpv9TFr8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+https://unsplash.com/pt/fotografias/M1y4TFQ9zVk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+https://unsplash.com/pt/fotografias/fdlZBWIP0aM?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+*/
