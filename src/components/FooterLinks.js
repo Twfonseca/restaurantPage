533a236fddd8da645f459937c605dc4e84bad0b6
Tawan Fonseca <webdevtawanfonseca@gmail.com>
2023-01-19 14:24:07 -0300

    all images selected, basic structure done

---
+++ src/components/FooterLinks.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import React from 'react'
+
+function FooterLinks(){
+    return(
+        ''
+    )
+}
+
+export default FooterLinks
---
+++ src/components/MenuFood.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import React from 'react'
+
+function MenuFood(){
+    return(
+        ''
+    )
+}
+
+export default MenuFood
