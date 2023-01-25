c655c20d7f6fda49dcc00c08a3077876ed9071bc
Tawan Fonseca <webdevtawanfonseca@gmail.com>
2023-01-25 00:45:03 -0300

    Building the menu actions. I started with the objects of each itens

--- src/components/MenuFood.js
+++ src/components/MenuFood.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import completeBreakfast from '../images/breakfast/completBreakfast.jpg'
 import kaakBreakfast from '../images/breakfast/kaak.jpg'
 import pancakeBreakfast from '../images/breakfast/pancake.jpg'
@@ -20,14 +20,11 @@ import pomeGranadeJuice from '../images/juices/pomegranade-juice.jpg'
 import baklawaSweet from '../images/sweet/Baklawa.jpg'
 
 
-
-
-
-
 /*  Logica do menu 
     1-  Adicionar as imagens em um array correspondente a sua area do menu
     2-  Criar efeito de transição para colocar no botão ao ativar a seção 
     3-  Fazer com que ao clicar no botão ativar a animação de transição  e adicionar a imagem ao style do background da seção 
+    4- Criar objetos com os valores  de cada prato para atribuilos dinamicament quando acionado algum dos botões 
     --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     Logica dos botões 
@@ -53,6 +50,121 @@ import baklawaSweet from '../images/sweet/Baklawa.jpg'
 */
 
 function MenuFood(){
+
+    const [breakfastItens, setBreakFastItens] =useState([
+        {   name:"Complete Breakfast", 
+            ingredients:"boiled eggs, pomegranate, grapes,brie cheese, Serrano jamon ham carpaccio, almonds, olives and figs",  description:"A fresh and healthy breakfast with an explosion of flavors ", 
+            price:"40,00", 
+            photo:completeBreakfast},
+        {   name:"Kaak", 
+            ingredients:"Flour, Sesame, Sugar, pomegranate syrup and milk", 
+            description:"A kind of donut of Mediterranean with a special and sweet flavor", 
+            price:"10,00", 
+            photo: kaakBreakfast},
+        {   name:"Pancake", 
+            ingredients:"flour, eggs, sugar, baking powder, vanilla ", 
+            description:" Three soft and tasty pancakes with honey and butter", 
+            price:"8,00", 
+            photo:pancakeBreakfast},
+        {   name:'Arugula salad',
+            ingredients:'Arugula, boiled egg and toast',
+            description:'A fresh arugula salad with a crunchy toast and two boiled eggs',
+            price:'15,00',
+            photo:saladEggBreakfast,
+        },
+        {   name:'Turkish coffee',
+            ingredients:'Special coffee,',
+            description:'A 250ml mug filled with a tasty and aromatic special coffee from Morocco',
+            price:'3,00',
+            photo: turkshCoffeBreakfast,
+        }])
+
+    const [lunchMenu, setLunchMenu] =useState([
+        {   name:'Curd',
+            ingredients:'milk and seasons ',
+            description:'delicious and fresh crud with toast',
+            price:'8,00',
+            photo:coalhadaLunch
+        }, 
+        {   name:'Homus',
+            ingredients:'chickpea, tahini and seasons  ',
+            description:'A delicious chickpea paste with toast',
+            price:'8,00',
+            photo:homusLunch
+        }, 
+        {   name:'Kebab with salad and crunchy',
+            ingredients:'Meat, tomatoes, chickpea, seasons and lettuce',
+            description:'A juicy roasted meat witha a delicious saland and roasted chickpea',
+            price:'25,00',
+            photo:kebabWithSalad
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:kebabAndSouces
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:kebabAndFries
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:saladLunch
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:completSaladLunch
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:hotSoup
+        }, 
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:chiliChicken
+        }])
+
+        const [juiceMenu, setJuiceMenu]= useState([
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:greenJuice
+        },
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:orangeJuice
+        },
+        {   name:'',
+            ingredients:'',
+            description:'',
+            price:'',
+            photo:pomeGranadeJuice
+        }])
+
+
+        const[sweetMenu, setSweetMenu]=useState(
+            {   name:'',
+                ingredients:'',
+                description:'',
+                price:'',
+                photo:baklawaSweet
+            })
+
+
     return(
         <section className='menuSecStyle'>
             <h1>Menu</h1>
@@ -63,6 +175,17 @@ function MenuFood(){
                 <button className='btnFoodsStyle'>Desserts</button>
                 
             </nav>
+            <div className='menuImgAndTextContainer'>
+                <div className='imgDisheContainer'>
+                            <img src={''}></img>
+                </div>
+                <div className=' disheDescriptionContainer'>
+                    <h4 className='textMenuFoodStyle'>Name</h4>
+                    <p className='textMenuFoodStyle'>Ingredients:</p>
+                    <p className='textMenuFoodStyle'>Description:</p>
+                    <p className='textMenuFoodStyle'>Price:</p>
+                </div>
+            </div>
             
         </section>
     )
