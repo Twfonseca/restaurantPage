faf2c4cc6f74ed717c73da1ecd4d19ea19df9603
Tawan Fonseca <webdevtawanfonseca@gmail.com>
2023-02-02 14:29:56 -0300

    Building the menu actions. Logial for the functions to change the dishes is started, breakfast and lunch done

--- src/components/MenuFood.js
+++ src/components/MenuFood.js
@@ -43,152 +43,177 @@ import baklawaSweet from '../images/sweet/Baklawa.jpg'
         AMBOS OS BOTÕES ( AVANÇAR E RETROCEDER ) IRÃO ADICIONAR UM BACKGROUND NA SEÇÃO CORRESPONDENTE AO ELEMENTO QUE ESTA SENDO VISUALIZADO 
 */
 
+/*   INSERIR EM CADA BOTÃO DE SEÇÃO DO MENU  UM IDENTIFICADOR PARA QUANDO CLICADO CARREGAR O VETOR DE IMAGENS QUE CORRESPONDE A SEÇÃO ADEQUADA  */
+
 function MenuFood(){
+
+    const [identifier, setIdentifier]=useState('')
+    const[quantityItens, setQuantity]=useState(0)
 
     const [breakfastItens, setBreakFastItens] =useState([
         {   name:"Complete Breakfast", 
             ingredients:"boiled eggs, pomegranate, grapes,brie cheese, Serrano jamon ham carpaccio, almonds, olives and figs",  description:"A fresh and healthy breakfast with an explosion of flavors ", 
-            price:"40,00", 
-            photo:completeBreakfast},
+            price:"40,00"
+        },
         {   name:"Kaak", 
             ingredients:"Flour, Sesame, Sugar, pomegranate syrup and milk", 
             description:"A kind of donut of Mediterranean with a special and sweet flavor", 
-            price:"10,00", 
-            photo: kaakBreakfast},
+            price:"10,00"
+        },
         {   name:"Pancake", 
             ingredients:"flour, eggs, sugar, baking powder, vanilla ", 
             description:" Three soft and tasty pancakes with honey and butter", 
-            price:"8,00", 
-            photo:pancakeBreakfast},
+            price:"8,00"
+        },
         {   name:'Arugula salad',
             ingredients:'Arugula, boiled egg and toast',
             description:'A fresh arugula salad with a crunchy toast and two boiled eggs',
-            price:'15,00',
-            photo:saladEggBreakfast,
+            price:'15,00'
         },
         {   name:'Turkish coffee',
             ingredients:'Special coffee,',
             description:'A 250ml mug filled with a tasty and aromatic special coffee from Morocco',
-            price:'3,00',
-            photo: turkshCoffeBreakfast,
+            price:'3,00'
         }])
+
 
     const [lunchMenu, setLunchMenu] =useState([
         {   name:'Curd',
             ingredients:'milk and seasons ',
             description:'delicious and fresh crud with toast',
-            price:'8,00',
-            photo:coalhadaLunch
+            price:'8,00'
         }, 
         {   name:'Homus',
             ingredients:'chickpea, tahini and seasons  ',
             description:'A delicious chickpea paste with toast',
-            price:'8,00',
-            photo:homusLunch
+            price:'8,00'
         }, 
         {   name:'Kebab with salad and crunchy',
             ingredients:'Meat, tomatoes, chickpea, seasons and lettuce',
             description:'A juicy roasted meat witha a delicious saland and roasted chickpea',
-            price:'25,00',
-            photo:kebabWithSalad
+            price:'25,00'
         }, 
         {   name:'Kebab with souce',
             ingredients:'Meat and special souces',
             description:'A juicy meat with special souces',
-            price:'20,00',
-            photo:kebabAndSouces
+            price:'20,00'
         }, 
         {   name:'Traditional Kebab',
             ingredients:'meat, potatoes, tomamotes and onion',
             description:'The traditional kebab meat with delicious fried potatoes',
-            price:'20,00',
-            photo:kebabAndFries
+            price:'20,00'
         }, 
         {   name:'purple lettuce',
             ingredients:'purple lettuce, fresh seasons',
             description:'A fresh purple lettuce salad with special seasons',
-            price:'10,00',
-            photo:saladLunch
+            price:'10,00'
         }, 
         {   name:'Complete Lunch',
             ingredients:'avocado, rice, almonds, mangos, Green cabbage, String bean, chickpea and onion',
             description:'A delicious and complet lunch with all the best flavors and seasons of Mediterranean',
-            price:'40,00',
-            photo:completSaladLunch
+            price:'40,00'
         }, 
         {   name:'Pepper Soup',
             ingredients:'Pepper, String bean, basil and boiled potatoe',
             description:'Warm and comfort-food with an unforgettable season ',
-            price:'20,00',
-            photo:hotSoup
+            price:'20,00'
         }, 
         {   name:'Hot chicken',
             ingredients:'chicken, pepper, milk and seasons',
             description:'A healthy soup with a confort-food taste',
-            price:'',
-            photo:chiliChicken
+            price:''
         }])
+
 
         const [juiceMenu, setJuiceMenu]= useState([
         {   name:'Green juice',
             ingredients:'Lemon, mint, spices and water',
             description:'A fresh and  healthy juice',
-            price:'4,00',
-            photo:greenJuice
+            price:'4,00'
         },
         {   name:'Orange juice',
             ingredients:'Orange, carrot, sugar and water',
             description:'The classic orange juice with chef is special ingredient ',
-            price:'4,00',
-            photo:orangeJuice
+            price:'4,00'
         },
         {   name:'Pomegranate juice',
             ingredients:'Pomegranate, water and sugar ',
             description:'Delicious and cold pomegranate juice',
-            price:'5,00',
-            photo:pomeGranadeJuice
+            price:'5,00'
         }])
-
-        const[menuIndex, setmenuIndex]=useState(0)
-
-            useEffect(()=>{
-                    loadBreakFast()
-            })
 
         const[sweetMenu, setSweetMenu]=useState(
             {   name:'Baklawa',
                 ingredients:'Filo pastry, nuts, butter, cinnamon, honey, water, vanilla',
                 description:'A classic Middle Eastern dessert. Sweet, crunchy and tasty',
                 price:'10,00',
-                photo:baklawaSweet
             })
 
-            let imageSelected
+            const[menuIndex, setmenuIndex]=useState(0)
+            const breakfastImages= [completeBreakfast, kaakBreakfast, pancakeBreakfast, saladEggBreakfast, turkshCoffeBreakfast]
+            const lunchMenuImages=[coalhadaLunch, homusLunch, kebabWithSalad, kebabAndSouces, kebabAndFries, saladLunch,
+            completSaladLunch, hotSoup, chiliChicken]
+            const juicesMenuImages =[greenJuice, orangeJuice, pomeGranadeJuice]
 
+            const indentifiers={
+                breakfast:breakfastImages,
+                lunch:lunchMenuImages,
+                juices:juicesMenuImages,
+                sweet:baklawaSweet
+            }
+
+            useEffect(()=>{
+                loadBreakFast()
+                })
 
         function loadBreakFast(){
             let foodName= document.querySelector("#disheName")
             let foodIngradients= document.querySelector("#disheIngredients")
             let foodDescription= document.querySelector("#disheDescription")
             let foodPrice= document.querySelector("#dishePrice")
+            setIdentifier('breakfast')
+            setQuantity(4)
 
             foodName.innerText=breakfastItens[menuIndex].name
             foodIngradients.innerText= "Ingredients: "+breakfastItens[menuIndex].ingredients
             foodDescription.innerText= "Description: "+breakfastItens[menuIndex].description
             foodPrice.innerText="Price: "+breakfastItens[menuIndex].price
-            imageSelected= breakfastItens[menuIndex].photo 
+        }
 
+        function loadLunch(){
+            let foodName= document.querySelector("#disheName")
+            let foodIngradients= document.querySelector("#disheIngredients")
+            let foodDescription= document.querySelector("#disheDescription")
+            let foodPrice= document.querySelector("#dishePrice")
+            setIdentifier('lunch')
+            setQuantity(5)
+
+            foodName.innerText=lunchMenu[menuIndex].name
+            foodIngradients.innerText= "Ingredients: "+lunchMenu[menuIndex].ingredients
+            foodDescription.innerText= "Description: "+lunchMenu[menuIndex].description
+            foodPrice.innerText="Price: "+lunchMenu[menuIndex].price
         }
 
         function nextItem(){
-            console.log(menuIndex)
-            if(menuIndex > 3){
-            setmenuIndex(0)
-            }
+            if(menuIndex < quantityItens){
             setmenuIndex(menuIndex+1)
-
-
+                }
+            if(menuIndex ===quantityItens){
+                setmenuIndex(0)
+            }
         }
+
+        function previousItem(){
+            if(menuIndex > 0){
+                setmenuIndex(menuIndex-1)
+            }
+            if(menuIndex === 0){
+                setmenuIndex(menuIndex+4)
+            }
+        }
+
+
+
 
 
 
@@ -197,13 +222,13 @@ function MenuFood(){
             <h1>Menu</h1>
             <nav className='menuFoodsStyle'>
                 <button className='btnFoodsStyle'onClick={loadBreakFast}>Breakfast</button>
-                <button className='btnFoodsStyle'>Lunch</button>
+                <button className='btnFoodsStyle'onClick={loadLunch}>Lunch</button>
                 <button className='btnFoodsStyle'>Juices</button>
                 <button className='btnFoodsStyle'>Desserts</button>    
             </nav>
             <div className='menuImgAndTextContainer'>
                 <div className='imgDisheContainer'>
-                            <img id='disheImage'src={imageSelected}></img>
+                            <img id='disheImage'src={''}></img>
                 </div>
                 <div className=' disheDescriptionContainer'>
                     <h4 className='textMenuFoodStyle' id='disheName'>Name</h4>
@@ -213,15 +238,12 @@ function MenuFood(){
                 </div>
             </div>
             <div className='btnsContainerOfMenu'>
-                <button className='menuItensChange'>Previous</button> 
+                <button className='menuItensChange' onClick={previousItem}>Previous</button> 
                 <button className='menuItensChange' onClick={nextItem}>Next</button>
             </div>
             
         </section>
     )
 }
-
-
-/* Adicionar todas as  imagens em um array para fazer  para fazer com que elas aparessam de acordo com o menu selecionado. Fazer com que essas mesmas imagens aparessam como back ground da section menu pra diferenciar as areas */
 
 export default MenuFood
